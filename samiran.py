2c7b659f3897b0c40bb5134e80885eb110da5a43
roy9382 <137901519+roy9382@users.noreply.github.com>
2023-07-01 10:36:05 +0530

    samiran.py

---
+++ samiran.py
@@ -0,0 +1,14 @@
+def main():
+    try:
+        age = int(input("Enter your age: "))
+        if age < 18:
+            print("You are a minor")
+        elif age >= 18 and age < 65:
+            print("You are an adult")
+        else:
+            print("You are a senior")
+    except ValueError:
+        print("Invalid input. Please enter a valid age.")
+
+if __name__ == "__main__":
+    main()
